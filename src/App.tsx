e47283102ba47d3ccd9b48694fdc35cfa536a10a
fabricio <fabriciobonete@yahoo.com.br>
2024-08-19 15:11:44 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+import Hero from './components/Hero';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <>   
+      <Header/>
+      <Hero/>    
+      <Footer/>
+    </>
+  )
+}
+
+export default App
